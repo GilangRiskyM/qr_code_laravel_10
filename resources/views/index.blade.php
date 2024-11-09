415282ea8890021d52c14ee1b0838d978dfb4ece
Gilang Risky M <gilangriskymahardhika@gmail.com>
2024-11-09 22:34:54 +0700

    tambah vercel

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,7 +8,7 @@
     <title>QR Code Generator</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="shortcut icon" href="assets/img/logo.png" type="image/x-icon">
+    <link rel="shortcut icon" href="https://gilangriskym.github.io/portfolioku/img/logo.png" type="image/x-icon">
 </head>
 
 <body>
